6d440a407a1b4f71a9e00fee5e2964900c9ab52c
Ismael Reyes <ismael.kix2902@gmail.com>
2022-12-01 20:06:03 +0100

    Add solutions for day 1 puzzles

---
+++ src/main/kotlin/y2022/d1/puzzle1.kt
@@ -0,0 +1,30 @@
+package y2022.d1
+
+import BasePuzzle
+import java.math.BigInteger
+
+object Puzzle1 : BasePuzzle {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val data = readFile(1)
+
+        var pos = 0
+        val list = mutableListOf<BigInteger>()
+        data.forEach { line ->
+            if (line.isBlank()) {
+                pos++
+            } else {
+                val num = BigInteger(line)
+                val value = (list.getOrNull(pos) ?: BigInteger.ZERO) + num
+                if (list.size == pos) {
+                    list.add(value)
+                } else {
+                    list[pos] = value
+                }
+            }
+        }
+
+        println(list.max())
+    }
+}
