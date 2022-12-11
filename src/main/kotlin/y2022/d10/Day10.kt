2364bff9a334878a7e62cfe8c87016adf7057476
Ismael Reyes <ismael.kix2902@gmail.com>
2022-12-11 16:04:00 +0100

    Added solution for 2022/10 puzzle 2

--- src/main/kotlin/y2022/d10/Day10.kt
+++ src/main/kotlin/y2022/d10/Day10.kt
@@ -1,6 +1,7 @@
 package y2022.d10
 
 import BasePuzzle
+import kotlin.math.abs
 
 object Day10 : BasePuzzle {
 
@@ -12,6 +13,10 @@ object Day10 : BasePuzzle {
 
         checkResponse(13140, puzzle1(test))
         println("Puzzle1: " + puzzle1(input).toString())
+
+        println("Puzzle2:")
+        puzzle2(test)
+        puzzle2(input)
 
         println()
     }
@@ -49,5 +54,45 @@ object Day10 : BasePuzzle {
         }
 
         return sumSignal
+    }
+
+    private fun puzzle2(data: List<String>) {
+        var cycle = 0
+        var value = 1
+        val pixels = (0..6).associateWith { "" }.toMutableMap()
+        var row = 0
+        data.forEach { line ->
+            if (line.startsWith("noop")) {
+                pixels[row] += if (abs(cycle - value) <= 1) "#" else " "
+                cycle++
+
+                if ((cycle == 40)) {
+                    cycle = 0
+                    row++
+                }
+
+            } else if (line.startsWith("addx")) {
+                val toAdd = line.substringAfter(" ").toInt()
+                pixels[row] += if (abs(cycle - value) <= 1) "#" else " "
+                cycle++
+
+                if ((cycle == 40)) {
+                    cycle = 0
+                    row++
+                }
+
+                pixels[row] += if (abs(cycle - value) <= 1) "#" else " "
+                cycle++
+
+                if ((cycle == 40)) {
+                    cycle = 0
+                    row++
+                }
+
+                value += toAdd
+            }
+        }
+
+        println(pixels.values.joinToString("\n"))
     }
 }
